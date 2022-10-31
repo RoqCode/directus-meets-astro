fa9dc0e7c4fd69281823704418469707e1a2d1f3
Hajo Haas <h.haas@webbrand.de>
2022-10-31 10:29:08 +0100

    scss rebuild with @use rules

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,12 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 import image from '@astrojs/image';
 
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -14,4 +20,20 @@ export default defineConfig({
       serviceEntryPoint: '@astrojs/image/sharp',
     }),
   ],
+
+  // vite: {
+  //   resolve: {
+  //     alias: {
+  //       '@/': `${path.resolve(__dirname, 'src')}/`,
+  //     },
+  //   },
+  //   css: {
+  //     preprocessorOptions: {
+  //       scss: {
+  //         // path to your scss variables
+  //         additionalData: `@import "@/styles/index.scss";`,
+  //       },
+  //     },
+  //   },
+  // },
 });
